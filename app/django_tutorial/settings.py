b76667e151921fb75b3f21e0c111fb9d57d1ef0f
Ivan Ruiperez <ivanruiperez.instituto@gmail.com>
2025-03-04 12:46:19 +0100

    nuevos

--- app/django_tutorial/settings.py
+++ app/django_tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["172.22.202.51","django.ivanruiperez.org"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['NAME'],
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': os.environ['HOST'],
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -121,3 +124,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+CSRF_TRUSTED_ORIGINS = [os.environ['URL']]
